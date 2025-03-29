eb0c0e9cb5695076b504c6ac31610f89bb99669a
Mouad Hachemi <mouad.hachemi2k@gmail.com>
2025-03-29 10:05:26 +0100

    Finished the initial design of the home page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import express from 'express';
+import { title } from 'process';
 
 // Set absolute path of the current working directory.
 const __dirname = path.resolve();
@@ -16,7 +17,22 @@ app.set('view engine', 'ejs');
 const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', {message: 'مرحبا بك على شاهين'});
+    const allPosts = [
+        {
+            title: 'منشور 1',
+            content: 'هذا النص يمكن أن يتم تركيبه على أي تصميم دون مشكلة فلن يبدو وكأنه نص منسوخ، غير منظم، غير منسق، أو حتى غير مفهوم.لأنه مازال نصاً بديلاً ومؤقتاً.',
+        },
+        {
+            title: 'منشور 2',
+            content: 'هذا النص يمكن أن يتم تركيبه على أي تصميم دون مشكلة فلن يبدو وكأنه نص منسوخ، غير منظم، غير منسق، أو حتى غير مفهوم.لأنه مازال نصاً بديلاً ومؤقتاً.',
+        },
+        {
+            title: 'منشور 3',
+            content: 'هذا النص يمكن أن يتم تركيبه على أي تصميم دون مشكلة فلن يبدو وكأنه نص منسوخ، غير منظم، غير منسق، أو حتى غير مفهوم.لأنه مازال نصاً بديلاً ومؤقتاً.',
+        }
+
+    ];
+    res.render('index.ejs', { allPosts });
 });
 
 app.listen(PORT, () => {
