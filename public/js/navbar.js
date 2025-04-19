af339bd47fbe8e5605aa09c1b388aa747d4d2f2b
Mouad Hachemi <mouad.hachemi2k@gmail.com>
2025-04-19 18:33:43 +0100

    Fixed a strange behaviour when navigating between categories pages

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,11 +3,8 @@ const categoriesLinks = document.getElementsByClassName("category-link");
 for (let item of categoriesLinks) {
     item.addEventListener('click', (event) => {
         event.preventDefault();
-
+        
         const category = item.id;
-        const urlParams = new URLSearchParams();
-        urlParams.append("category", category);
-
-        window.location.search = urlParams;
+        window.location.href = `/?category=${category}`;
     });
 }
