48ddc7e64d49a52a90f218375435790e7157e4dc
Mouad Hachemi <mouad.hachemi2k@gmail.com>
2025-04-23 21:41:59 +0100

    Responsive Styling

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,8 +3,15 @@ const categoriesLinks = document.getElementsByClassName("category-link");
 for (let item of categoriesLinks) {
     item.addEventListener('click', (event) => {
         event.preventDefault();
-        
+
         const category = item.id;
         window.location.href = `/?category=${category}`;
     });
 }
+
+const menuBtn = document.getElementById('menuBtn');
+const navLinks = document.getElementById('navLinks');
+
+menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
